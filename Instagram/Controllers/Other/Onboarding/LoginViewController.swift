4b7a6243561d6f7643a6b02be4bd1aed494419b3
ShadowPlayer34 <khudzik24@mail.ru>
2023-02-12 16:26:01 +0300

    made preparations

---
+++ Instagram/Controllers/Other/Onboarding/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by Андрей Худик on 12.02.23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
