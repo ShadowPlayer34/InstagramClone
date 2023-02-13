e0e7f57f547845a22214e0b03ff13d32554d2ae5
ShadowPlayer34 <khudzik24@mail.ru>
2023-02-13 17:51:10 +0300

    Create pod

---
+++ Instagram/Resources/AuthManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthManager.swift
+//  Instagram
+//
+//  Created by Андрей Худик on 13.02.23.
+//
+
+import Foundation
---
+++ Instagram/Resources/StorageManager.swift
@@ -0,0 +1,8 @@
+//
+//  StorageManager.swift
+//  Instagram
+//
+//  Created by Андрей Худик on 13.02.23.
+//
+
+import Foundation
