3c8170248cd1825daf0f51c565e3896352f573cb
ShadowPlayer34 <khudzik24@mail.ru>
2023-02-14 17:57:36 +0300

    added extension for UIView

--- Instagram/Resources/Extensions.swift
+++ Instagram/Resources/Extensions.swift
@@ -5,4 +5,30 @@
 //  Created by Андрей Худик on 14.02.23.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    public var width: CGFloat {
+        return frame.width
+    }
+    
+    public var height: CGFloat {
+        return frame.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.height
+    }
+    
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var rigth: CGFloat {
+        return frame.origin.x + frame.width
+    }
+}
