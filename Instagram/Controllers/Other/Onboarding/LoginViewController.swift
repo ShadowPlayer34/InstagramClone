0821aabb9a3d74cd5c5fb3ffa6386794d0305e4f
ShadowPlayer34 <khudzik24@mail.ru>
2023-02-14 08:57:30 +0300

    start doing login view

--- Instagram/Controllers/Other/Onboarding/LoginViewController.swift
+++ Instagram/Controllers/Other/Onboarding/LoginViewController.swift
@@ -9,9 +9,61 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    private let emailUsernameTextField: UITextField = {
+        return UITextField()
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let field = UITextField()
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let createAccountButton: UIButton = {
+        return UIButton()
+    }
+    
+    private let termsButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let privacyButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let logoView: UIView = {
+        return UIView()
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        addSubviews()
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        // asign frames
+    }
+    
+    private func addSubviews() {
+        view.addSubview(emailUsernameTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(loginButton)
+        view.addSubview(createAccountButton)
+        view.addSubview(termsButton)
+        view.addSubview(privacyButton)
+        view.addSubview(logoView)
+    }
+    
+    @objc private func loginButtonTapped() {}
+    @objc private func createAccountButtonTapped() {}
+    @objc private func termsButtonTapped() {}
+    @objc private func privacyButtonTapped() {}
 
 }
