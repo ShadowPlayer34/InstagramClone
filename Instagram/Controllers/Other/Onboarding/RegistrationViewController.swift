e4105697d2d9065b049b6cf7bcd132df68a75fed
ShadowPlayer34 <khudzik24@mail.ru>
2023-02-15 17:08:39 +0300

    completed registration VC

--- Instagram/Controllers/Other/Onboarding/RegistrationViewController.swift
+++ Instagram/Controllers/Other/Onboarding/RegistrationViewController.swift
@@ -8,22 +8,127 @@
 import UIKit
 
 class RegistrationViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    struct Constsants {
+        static var cornerRadius: CGFloat = 8.0
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private let usernameTextField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Username..."
+        field.returnKeyType = .next
+        field.layer.cornerRadius = Constsants.cornerRadius
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.secondaryLabel.cgColor
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.autocapitalizationType = .none
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        return field
+    }()
+    
+    private let emailTextField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Email Adress..."
+        field.returnKeyType = .next
+        field.layer.cornerRadius = Constsants.cornerRadius
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.secondaryLabel.cgColor
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.autocapitalizationType = .none
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        return field
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let field = UITextField()
+        field.isSecureTextEntry = true
+        field.placeholder = "Password..."
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = Constsants.cornerRadius
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.secondaryLabel.cgColor
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.autocapitalizationType = .none
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        return field
+    }()
+    
+    private let registerButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemGreen
+        button.setTitle("Sign Up", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = Constsants.cornerRadius
+        return button
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        usernameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        registerButton.addTarget(self,
+                                 action: #selector(registerButtonTapped),
+                                 for: .touchUpInside)
+        addSubviews()
     }
-    */
+    
+    private func addSubviews() {
+        view.addSubview(usernameTextField)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(registerButton)
+    }
+    
+    @objc private func registerButtonTapped() {
+        view.endEditing(true)
+        
+        guard let username = usernameTextField.text, !username.isEmpty,
+              let email = emailTextField.text, !email.isEmpty,
+              let password = passwordTextField.text, password.count >= 8 else {
+                  return
+              }
+        
+        AuthManager.shared.registerNewUser(username: username, email: email, password: password) { registered in
+            DispatchQueue.main.async {
+                if registered {
+                    // good to go
+                } else {
+                    // failed
+                }
+            }
+        }
+    }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        usernameTextField.frame = CGRect(x: 20, y: view.safeAreaInsets.top + 30, width: view.width - 40, height: 52)
+        emailTextField.frame = CGRect(x: 20, y: usernameTextField.bottom + 10, width: view.width - 40, height: 52)
+        passwordTextField.frame = CGRect(x: 20, y: emailTextField.bottom + 10, width: view.width - 40, height: 52)
+        registerButton.frame = CGRect(x: 20, y: passwordTextField.bottom + 10, width: view.width - 40, height: 52)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+}
+
+extension RegistrationViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == usernameTextField {
+            emailTextField.becomeFirstResponder()
+        } else if textField == emailTextField {
+            passwordTextField.becomeFirstResponder()
+        } else {
+            registerButtonTapped()
+        }
+        return true
+    }
 }
