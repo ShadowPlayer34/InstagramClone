0ff86789c8a17bdc52e2ab2d6dc43424e6874b5c
ShadowPlayer34 <khudzik24@mail.ru>
2023-02-15 17:11:17 +0300

    maded insert new user into database

--- Instagram/Resources/DatabaseManager.swift
+++ Instagram/Resources/DatabaseManager.swift
@@ -10,5 +10,29 @@ import FirebaseDatabase
 
 public class DatabaseManager {
     static let shared = DatabaseManager()
+    let database = Database.database().reference()
     
+    /// Check if username and email are avaliable
+    /// - Parameters:
+    ///  - email: String represanting email
+    ///  - password: String represanting password
+    public func canCreateNewUser(with email: String, username: String, completion: (Bool) -> Void) {
+        completion(true)
+    }
+    
+    /// Insert new user data to database
+    /// - Parameters:
+    ///  - email: String represanting email
+    ///  - password: String represanting password
+    public func insertNewUser(with email: String, username: String, completion: @escaping (Bool) -> Void) {
+        database.child(username).setValue(["email": email]) { error, _ in
+            if error == nil {
+                completion(true)
+                return
+            } else {
+                completion(false)
+                return
+            }
+        }
+    }
 }
