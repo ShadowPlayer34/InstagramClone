0e246f26a0fbdb4d981dd0cdbb43834414df66be
ShadowPlayer34 <khudzik24@mail.ru>
2023-02-17 15:10:01 +0300

    madded settings vc and add log out posibility

--- Instagram/Controllers/Other/SettingsViewController.swift
+++ Instagram/Controllers/Other/SettingsViewController.swift
@@ -7,23 +7,98 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+struct SettingsCellModel {
+    let title: String
+    let handler: (() -> Void)
+}
+
+/// View Controller to show user settings
+final class SettingsViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero,
+                                style: .grouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
+    
+    var data = [[SettingsCellModel]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureModels()
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
-    */
+    
+    private func configureModels() {
+        let section = [
+            SettingsCellModel(title: "Log Out", handler: { [weak self] in
+                self?.didTapLogOut()
+            })
+        ]
+        data.append(section)
+    }
+    
+    private func didTapLogOut() {
+        let actionSheet = UIAlertController(title: "Log Out",
+                                            message: "Are you sure you want to log out?",
+                                            preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Cancel",
+                                     style: .cancel,
+                                     handler: nil))
+        let okButton = UIAlertAction(title: "Log Out", style: .destructive) { _ in
+            AuthManager.shared.logOut { success in
+                DispatchQueue.main.async {
+                    if success {
+                        //present log in
+                        let loginVC = LoginViewController()
+                        loginVC.modalPresentationStyle = .fullScreen
+                        self.present(loginVC, animated: true) {
+                            self.navigationController?.popToRootViewController(animated: false)
+                            self.tabBarController?.selectedIndex = 0
+                        }
+                    } else {
+                        // error occured
+                        fatalError("Could not log out user")
+                    }
+                }
+            }
+        }
+        actionSheet.addAction(okButton)
+        
+        actionSheet.popoverPresentationController?.sourceView = tableView
+        actionSheet.popoverPresentationController?.sourceRect = tableView.bounds
+        present(actionSheet, animated: true)
+    }
+}
 
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data[section].count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else { fatalError() }
+        cell.textLabel?.text = data[indexPath.section][indexPath.row].title
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // Handle cell selection here
+        data[indexPath.section][indexPath.row].handler()
+    }
 }
