53c8316be2984651cdf0cef32880ac27b0c8f2a6
ShadowPlayer34 <khudzik24@mail.ru>
2023-03-03 16:05:14 +0300

    added tableView and registered cells

--- Instagram/Controllers/Core Tabs/HomeViewController.swift
+++ Instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -8,20 +8,68 @@
 import UIKit
 import FirebaseAuth
 
+struct HomeFeedRenderViewModel {
+    let header: PostRenderViewModel
+    let post: PostRenderViewModel
+    let actions: PostRenderViewModel
+    let comments: PostRenderViewModel
+}
+
 class HomeViewController: UIViewController {
 
+    private var feedRenderModels = [HomeFeedRenderViewModel]()
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(IGFeedPostsTableViewCell.self,
+                       forCellReuseIdentifier: IGFeedPostsTableViewCell.identifier)
+        table.register(IGFeedPostHeaderTableViewCell.self,
+                       forCellReuseIdentifier: IGFeedPostHeaderTableViewCell.identifier)
+        table.register(IGFeedPostActionsTableViewCell.self,
+                       forCellReuseIdentifier: IGFeedPostActionsTableViewCell.identifier)
+        table.register(IGFeedPostGeneralTableViewCell.self,
+                       forCellReuseIdentifier: IGFeedPostGeneralTableViewCell.identifier)
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         handleNotAuthenticated()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
 
     func handleNotAuthenticated() {
         if Auth.auth().currentUser == nil {
             let loginVC = LoginViewController()
             loginVC.modalPresentationStyle = .fullScreen
-            present(loginVC, animated: true)
+            present(loginVC, animated: false)
         }
     }
-
 }
 
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostsTableViewCell.identifier,
+                                                 for: indexPath) as! IGFeedPostsTableViewCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
