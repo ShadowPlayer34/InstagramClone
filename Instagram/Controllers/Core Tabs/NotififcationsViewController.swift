7443c6e00f2b0cfb08762ff074f74d55e3ac5d2c
ShadowPlayer34 <khudzik24@mail.ru>
2023-03-03 16:06:12 +0300

    completed notificationVC

--- Instagram/Controllers/Core Tabs/NotififcationsViewController.swift
+++ Instagram/Controllers/Core Tabs/NotififcationsViewController.swift
@@ -7,23 +7,141 @@
 
 import UIKit
 
-class NotififcationsViewController: UIViewController {
+enum NotificationType {
+    case like(post: UserPost)
+    case follow(state: FollowState)
+}
 
+struct UserNotification {
+    let type: NotificationType
+    let text: String
+    let user: User
+}
+
+final class NotififcationsViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.isHidden = false
+        table.register(NotificationLikeEventTableViewCell.self,
+                       forCellReuseIdentifier: NotificationLikeEventTableViewCell.identifier)
+        table.register(NotficationFollowEventTableViewCell.self,
+                       forCellReuseIdentifier: NotficationFollowEventTableViewCell.identifier )
+        return table
+    }()
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.tintColor = .label
+        return spinner
+    }()
+    
+    private lazy var noNotificationsView = NoNotificationView()
+    private var models = [UserNotification]()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        fetchNotifications()
+        navigationItem.title = "Notifications"
+        view.backgroundColor = .systemBackground
+        view.addSubview(spinner)
+        view.addSubview(tableView)
+//        spinner.startAnimating()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        // FIXME: problem with auto transparent bars, that's why frame equal safe area
+        tableView.frame = view.safeAreaLayoutGuide.layoutFrame
+        spinner.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        spinner.center = view.center
     }
-    */
+    
+    private func fetchNotifications() {
+        for x in 0...100 {
+            let user = User(username: "@joe",
+                            bio: "",
+                            name: ("", ""),
+                            profilePhoto: URL(string: "hhtps://www.google.com")!,
+                            birthDate: Date(),
+                            gender: .male,
+                            counts: UserCount(followers: 45, following: 5, posts: 4),
+                            joinDate: Date())
+            let post = UserPost(identifier: "", postType: .photo,
+                                thumbnailImage: URL(string: "https://www.google.com")!,
+                                postURL: URL(string: "https://www.google.com")!,
+                                caption: nil,
+                                likeCount: [],
+                                comments: [],
+                                createdDate: Date(),
+                                taggedUsers: [],
+                                owner: user)
+            let model = UserNotification(type: x % 2 == 0 ? .follow(state: .following) : .like(post: post),
+                                         text: "Hello World!",
+                                         user: user)
+            models.append(model)
+        }
+    }
+    
+    private func addNoNotifications() {
+        tableView.isHidden = true
+        view.addSubview(noNotificationsView)
+        noNotificationsView.frame = CGRect(x: 0, y: 0, width: view.width / 2, height: view.width / 4)
+        noNotificationsView.center = view.center
+    }
+}
 
+extension NotififcationsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        switch model.type {
+        case .like(_):
+            let cell = tableView.dequeueReusableCell(withIdentifier: NotificationLikeEventTableViewCell.identifier, for: indexPath) as! NotificationLikeEventTableViewCell
+            cell.configure(with: model)
+            cell.delegate = self
+            return cell
+        case .follow:
+            let cell = tableView.dequeueReusableCell(withIdentifier: NotficationFollowEventTableViewCell.identifier, for: indexPath) as! NotficationFollowEventTableViewCell
+            cell.configure(with: model)
+            cell.delegate = self
+            return cell
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 52
+    }
+}
+
+extension NotififcationsViewController: NotificationLikeEventTableViewCellDelegate, NotificationFollowEventTableViewCellDelegate {
+    func didTapRelatedPostButton(model: UserNotification) {
+        switch model.type {
+        case .like(let post):
+            let vc = PostViewController(model: post)
+            vc.title = post.postType.rawValue
+            vc.navigationItem.largeTitleDisplayMode = .never
+            navigationController?.pushViewController(vc, animated: true)
+        case .follow(_):
+            fatalError("Dev Issue: Should never get called")
+        }
+    }
+    
+    func didTapFollowButton(model: UserNotification) {
+        print("Tapped follow")
+        // perfom databasse update
+    }
 }
