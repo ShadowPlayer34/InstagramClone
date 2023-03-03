0659255fd99edf1618e1065324d8d4cb628b395f
ShadowPlayer34 <khudzik24@mail.ru>
2023-03-03 16:37:59 +0300

    addded tableview, subviews, put they

--- Instagram/Controllers/Other/PostViewController.swift
+++ Instagram/Controllers/Other/PostViewController.swift
@@ -7,23 +7,142 @@
 
 import UIKit
 
+/*
+ 
+ Section
+ - Header model
+ Section
+ - Post Cell model
+ Section
+ - Action Buttons Cell moddel
+ Section
+ - n Number of general models for comments
+ 
+ */
+
+/// States of a rendered cell
+enum PostRenderType {
+    case header(provider: User)
+    case primaryContent(provider: UserPost)
+    case actions(provider: String)
+    case comments(provider: [PostComment])
+}
+
+/// Model of rendered posts
+struct PostRenderViewModel {
+    let renderType: PostRenderType
+}
+
 class PostViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    private let model: UserPost?
+    private var renderModels = [PostRenderViewModel]()
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(IGFeedPostsTableViewCell.self,
+                       forCellReuseIdentifier: IGFeedPostsTableViewCell.identifier)
+        table.register(IGFeedPostHeaderTableViewCell.self,
+                       forCellReuseIdentifier: IGFeedPostHeaderTableViewCell.identifier)
+        table.register(IGFeedPostActionsTableViewCell.self,
+                       forCellReuseIdentifier: IGFeedPostActionsTableViewCell.identifier)
+        table.register(IGFeedPostGeneralTableViewCell.self,
+                       forCellReuseIdentifier: IGFeedPostGeneralTableViewCell.identifier)
+        return table
+    }()
+    
+    init(model: UserPost?) {
+        self.model = model
+        super.init(nibName: nil, bundle: nil)
+        setUpRenderModel()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    private func setUpRenderModel() {
+        guard let userPostModel = self.model else { return }
+        // Header
+        renderModels.append(PostRenderViewModel(renderType: .header(provider: userPostModel.owner)))
+        // Primary
+        renderModels.append(PostRenderViewModel(renderType: .primaryContent(provider: userPostModel)))
+        // Actions
+        renderModels.append(PostRenderViewModel(renderType: .actions(provider: "")))
+        //Comments
+        var comments = [PostComment]()
+        for x in 1...4 {
+            comments.append(PostComment(identifier: "123_\(x)",
+                                        username: "@joe\(x)",
+                                        text: "Hello",
+                                        createdDate: Date(),
+                                        likes: []))
+        }
+        renderModels.append(PostRenderViewModel(renderType: .comments(provider: comments)))
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.backgroundColor = .systemBackground
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+}
 
+extension PostViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return renderModels.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch renderModels[section].renderType {
+        case .actions(_): return 1
+        case .comments(let comments): return comments.count > 4 ? 4 : comments.count
+        case .primaryContent(_): return 1
+        case .header(_): return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = renderModels[indexPath.section]
+        switch model.renderType {
+        case .actions(let actions):
+            let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostActionsTableViewCell.identifier,
+                                                     for: indexPath) as! IGFeedPostActionsTableViewCell
+            return cell
+            
+        case .comments(let comments):
+            let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostGeneralTableViewCell.identifier,
+                                                     for: indexPath) as! IGFeedPostGeneralTableViewCell
+            return cell
+            
+        case .primaryContent(let post):
+            let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostsTableViewCell.identifier,
+                                                     for: indexPath) as! IGFeedPostsTableViewCell
+            return cell
+            
+        case .header(let user):
+            let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostHeaderTableViewCell.identifier,
+                                                     for: indexPath) as! IGFeedPostHeaderTableViewCell
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let model = renderModels[indexPath.section]
+        switch model.renderType {
+        case .actions(_): return 60
+        case .comments(_): return 50
+        case .primaryContent(_): return tableView.width
+        case .header(_): return 70
+        }
+    }
 }
