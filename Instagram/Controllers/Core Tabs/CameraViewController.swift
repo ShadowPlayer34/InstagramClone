8f1a4403e8fec6708365df89d2f05fba1265b1c0
ShadowPlayer34 <khudzik24@mail.ru>
2023-03-12 00:43:19 +0300

    added camera and privacy for camera

--- Instagram/Controllers/Core Tabs/CameraViewController.swift
+++ Instagram/Controllers/Core Tabs/CameraViewController.swift
@@ -8,11 +8,43 @@
 import UIKit
 
 class CameraViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        configurePicker()
     }
+    
+    private func configurePicker() {
+        let picker = UIImagePickerController()
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+            picker.delegate = self
+            picker.allowsEditing = true
+            picker.modalPresentationStyle = .fullScreen
+            present(picker, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Problem with camera",
+                                          message: "You are camera is unavaliable",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+}
 
+extension CameraViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[.editedImage] as? UIImage else {
+            return
+        }
+        // make a new post
+    }
+    
+    
 }
