65f4ed6f837b75e1ed2c0a124bca01c60ec98bee
ShadowPlayer34 <khudzik24@mail.ru>
2023-03-12 13:02:16 +0300

    added and configurated collection view and dimmed view

--- Instagram/Controllers/Core Tabs/ExploreViewController.swift
+++ Instagram/Controllers/Core Tabs/ExploreViewController.swift
@@ -11,23 +11,61 @@ class ExploreViewController: UIViewController {
 
     private var searchBar: UISearchBar = {
         let bar = UISearchBar()
+        bar.placeholder = "Search"
         bar.backgroundColor = .secondarySystemBackground
         return bar
     }()
     
+    private var model = [UserPost]()
+    
     private var collectionView: UICollectionView?
+    
+    private let dimmedView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.alpha = 0.0
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.navigationBar.topItem?.titleView = searchBar
         view.backgroundColor = .systemBackground
-        
+        configureSearchBar()
+        configureExploreCollection()
+        configureDimmedView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView?.frame = view.bounds
+        dimmedView.frame = view.bounds
+    }
+    
+    private func configureDimmedView() {
+        view.addSubview(dimmedView)
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(didCancelSearch))
+        gesture.numberOfTapsRequired = 1
+        gesture.numberOfTouchesRequired = 1
+        dimmedView.addGestureRecognizer(gesture)
+    }
+    
+    private func configureSearchBar() {
+        navigationController?.navigationBar.topItem?.titleView = searchBar
+        searchBar.delegate = self
+    }
+    
+    private func configureExploreCollection() {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-//        layout.itemSize = CGSize(width: <#T##Double#>, height: <#T##Double#>)
+        let size = (view.width - 4) / 3.0
+        layout.itemSize = CGSize(width: size, height: size)
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
         collectionView = UICollectionView(frame: .zero,
                                           collectionViewLayout: layout)
+        collectionView?.register(PhotoCollectionViewCell.self,
+                                 forCellWithReuseIdentifier: PhotoCollectionViewCell.identifire)
         collectionView?.delegate = self
         collectionView?.dataSource = self
         guard let collectionView = collectionView else { return }
@@ -35,16 +73,86 @@ class ExploreViewController: UIViewController {
     }
 }
 
+extension ExploreViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Cancel",
+            style: .plain,
+            target: self,
+            action: #selector(didCancelSearch)
+        )
+        dimmedView.isHidden = false
+        UIView.animate(withDuration: 0.2) {
+            self.dimmedView.alpha = 0.5
+        }
+    }
+    
+    @objc func didCancelSearch() {
+        searchBar.resignFirstResponder()
+        navigationItem.rightBarButtonItem = nil
+        UIView.animate(withDuration: 0.2) {
+            self.dimmedView.alpha = 0
+        } completion: { done in
+            if done {
+                self.dimmedView.isHidden = true
+            }
+        }
+
+       
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        didCancelSearch()
+        guard let text = searchBar.text else { return }
+         
+        query(text)
+    }
+    
+    private func query(_ text: String) {
+        // perform the search in the back end
+    }
+}
+
 extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return 100
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifire,
+                                                            for: indexPath) as! PhotoCollectionViewCell
+//        cell.configure(with: )
+        cell.configure(test: "test")
+        return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
+        let user = User(username: "@joe",
+                        bio: "",
+                        name: ("", ""),
+                        profilePhoto: URL(string: "hhtps://www.google.com")!,
+                        birthDate: Date(),
+                        gender: .male,
+                        counts: UserCount(followers: 45, following: 5, posts: 4),
+                        joinDate: Date())
+        let post = UserPost(identifier: "", postType: .photo,
+                            thumbnailImage: URL(string: "https://www.google.com")!,
+                            postURL: URL(string: "https://www.google.com")!,
+                            caption: nil,
+                            likeCount: [],
+                            comments: [],
+                            createdDate: Date(),
+                            taggedUsers: [],
+                            owner: user)
+        let vc = PostViewController(model: post)
+        vc.title = post.postType.rawValue
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
